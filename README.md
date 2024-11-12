ed8393050996ad3dbf5c20b3e2f29e16f8739b5a
aya ehab <ayaaehab1999@gmail.com>
2024-11-12 10:44:32 +0300

    stup veriy and sign token

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # express-api-jwt-auth-template
+
+- touch server.js .env .gitignore
+- npm init -y
+- npm i express mongoose dotenv morgan
+
+
--- server.js
+++ server.js
@@ -3,11 +3,16 @@ require('./Config/database');
 const express = require('express');
 const morgan = require('morgan');
 
+//controllers:
+const testJWTRouter = require('./controllers/test-jwt');
+
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
 // Routes go here
+app.use('/test-jwt', testJWTRouter);
+
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
