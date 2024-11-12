ece2e1cd5ab9ff980e94e458e67ec39536deb75a
aya ehab <ayaaehab1999@gmail.com>
2024-11-12 11:32:21 +0300

    finish the jwt sign in / sign up

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const router = express.Router();
@@ -15,11 +16,32 @@ router.post('/signup', async (req, res) => {
       username: req.body.username,
       hashedPassword: bcrypt.hashSync(req.body.password, parseInt(process.env.SALT_ROUNDS)),
     });
-    console.log(user);
-    res.status(201).json({ user });
+
+    const token = jwt.sign({ username: user.username, _id: user._id }, process.env.JWT_SECRET);
+
+    res.status(201).json({ user, token });
   } catch (error) {
     console.error(error);
     res.status(400).json({ error: 'Something Went wrong!' });
+  }
+});
+
+router.post('/signin', async (req, res) => {
+  try {
+    // look up user to see if they exist
+    const user = await User.findOne({ username: req.body.username });
+
+    // check if user is there and password is good
+    if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
+      const token = jwt.sign({ username: user.username, _id: user._id }, process.env.JWT_SECRET);
+
+      res.json({ token });
+    } else {
+      throw new Error('Invalid Credentials');
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: 'Invalid Credentials' });
   }
 });
 
